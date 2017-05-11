f3c97f26ad1076f4c808154a6ccee677a7b6614d
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-05-11 11:22:09 +0200

    with method added

--- general_exponent_differentaite.py
+++ general_exponent_differentaite.py
@@ -1,34 +1,32 @@
 from koneroj import*
 from differentiate import*
 from logarithmic_differentiation import* 
-general_exponent_differentiate = writable_theorem("General exponent differentaite")
-general_exponent_differentiate.parts['Result'].desplay +=r"""
-$$ 
+general_exponent_differentiate = writable_theorem("General exponent differentiate")
+with general_exponent_differentiate as x:
+    x.parts['Result'].desplay +=r""" $$ 
     y = a^{x} +C \quad 
     \Leftrightarrow \quad 
-     y \prime"""
-general_exponent_differentiate.parts["Result"].add_concept(differentiate)
-general_exponent_differentiate.parts['Result'].desplay +=r"""
- = ln(a)a^{x}
-$$
-"""
-
-general_exponent_differentiate.parts['Proof'].desplay +=r"""
+    y \prime"""
+    x.parts["Result"].add_concept(differentiate)
+    x.parts['Result'].desplay +=r"""
+     = ln(a)a^{x}
+    $$
+    """
+    x.parts['Proof'].desplay +=r"""
 
     \begin{align*}
-    y + C &= a^{x} \\
+   y + C &= a^{x} \\
+    """
+    x.parts['Proof'].add_technique(logarithmic_differentiation,r"""\Leftrightarrow""")
+    x.parts['Proof'].desplay +=r"""  \quad y  \prime  & = y (x \ln(a))\prime 
 """
-general_exponent_differentiate.parts['Proof'].add_technique(logarithmic_differentiation,r"""\Leftrightarrow""")
-general_exponent_differentiate.parts['Proof'].desplay +=r"""  \quad y  \prime  & = y (x \ln(a))\prime 
+    x.parts['Proof'].desplay +=r"""\\ & = y \ln(a)\\ 
 """
-general_exponent_differentiate.parts['Proof'].desplay +=r"""\\ & = y \ln(a)\\ 
-"""
-
-general_exponent_differentiate.parts['Proof'].desplay +=r"""     & = \ln(a) a^{x} 
+    x.parts['Proof'].desplay +=r"""     & = \ln(a) a^{x} 
     \end{align*}
     
 """
-general_exponent_differentiate.write_module_doc() 
+    x.write_module_doc() 
 
 
 
