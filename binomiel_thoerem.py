19755435ebb50c535cd4b46dade77d5900cbe649
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-05-15 16:02:11 +0200

    frame work set

--- binomiel_thoerem.py
+++ binomiel_thoerem.py
@@ -5,16 +5,19 @@ with binomiel_thoerem as x:
     x.parts["Proof"].desplay += r"""$$ p(n) \text{ is the stament }  \left( a + b \right) ^n = \sum_{i=0}^n {{n}\choose{i}} a^{n-i} b^{i}$$"""
     x.parts["Proof"].desplay += r"""$$ p(0):\mathbf{true} $$"""
     x.parts["Proof"].desplay += r"""$$ \text{Induction assumption} \quad  p(n) :\mathbf{true}$$
-$$ \left( a+b \right) ^{n+1}$$
-$$ = \left( a + b \right) \left( a + b \right) ^n $$
-$$ = a \left( a + b \right) ^n + b \left( a + b \right) ^n $$ 
-$$ = a \sum_{i=0}^n {{n}\choose{i}} a^{n-i} b^{i} + b \sum_{i=0}^n {{n}\choose{i}} a^{n-i} b^{i} $$
-$$ = \sum_{i=0}^n {{n}\choose{i}} a^{n +1- i} b^{i} + \sum_{i=0}^n {{n}\choose{i}} a^{n-i} b^{i+1} $$
-$$ = \sum_{i=0}^0 {{n}\choose{i}} a^{n+1-i} b^{i} \sum_{i=1}^n {{n}\choose{i}} a^{n+1-i} b^{i} + \sum_{i=0}^{n-1} {{n}\choose{i}} a^{n-i} b^{i+1} \sum_{i=n}^{n} {{n}\choose{i}} a^{n+1-i} b^{i} $$
-$$ = {{n}\choose{0}}  a^{n+1} \sum_{i=1}^n {{n}\choose{i}} a^{n+1-i} b^{i} + \sum_{i=0}^{n-1} {{n}\choose{i}} a^{n-i} b^{i+1}  {{n}\choose{n}} {n+1} $$
-$$ x^{n+1} + y^{n+1} + \sum_{i=1}^n {{n}\choose{i}} a^{n+1-i} b^{i} +  \sum_{i=0}^{n-1} {{n}\choose{i}} a^{n-i} b^{i+1} $$ 
-$$ {{n+1}\choose{0}}x^{n+1} + {{n+1} \choose {n+1}} +  \sum_{i=1}^n {{n+1}\choose{i}} a^{n+1-i} b^{i}  $$
-$$ = \sum_{i=0}^{n+1} {{n+1}\choose{i}} a^{n+1-i} b^{i}  $$
-
+\begin{align*}
+\left( a+b \right) ^{n+1} &= \left( a + b \right) \left( a + b \right) ^n \\
+        &= \left( a + b \right) \sum_{i=0}^n {{n}\choose{i}} a^{n-i} b^{i} \\
+    &= \sum_{i=0}^n {{n}\choose{i}} a^{n +1- i} b^{i} + \sum_{i=0}^n {{n}\choose{i}} a^{n-i} b^{i+1} \\
+    &= u+v 
+\end{align*}
+\textbf{Where}
+\begin{align*}
+u &= \sum_{i=0}^0 + {{n}\choose{i}} a^{n+1-i} b^{i} + \sum_{i=n}^    {n} {{n}\choose{i}} a^{n+1-i} b^{i} &  v &= \sum_{i=1}^n {{n}\choose{i}}     a^{n+1-i} b^{i} + \sum_{i=0}^{n-1} {{n}\choose{i}} a^{n-i} b^{i+1}\\
+&=a^{n+1}+b^{n+1} & &= \sum_{r=1}^n{{n}\choose{i}}a^{n-r+1}b^r
+\end{align*}
+\begin{align*}
+(a+b)^{n+1} &= \sum_{i=0}^{n+1} {{n+1}\choose{i}} a^{n+1-i} b^{i} 
+\end{align*}
 """
     x.write_module_doc()
