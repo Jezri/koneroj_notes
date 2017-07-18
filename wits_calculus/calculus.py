6ad4e1f8a5d401cb6abf8abb09b2c10a1e9ce7f5
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-07-18 18:07:06 +0200

    formating information now in koneroj.sty not in each file for calculus

--- wits_calculus/calculus.py
+++ wits_calculus/calculus.py
@@ -1,39 +1,37 @@
-from makebook import makebook
-list=[ '/home/jezri/wits_calculus/partition.tex',
-'/home/jezri/wits_calculus/partition_subinterval.tex',
-'/home/jezri/wits_calculus/norm_of_a_partition.tex',
-'/home/jezri/wits_calculus/riemann_sum.tex',
-'/home/jezri/wits_calculus/definite_integral.tex',
-'/home/jezri/wits_calculus/continues_functions_are_intergratable.tex'
-,'/home/jezri/wits_calculus/piecewise_continuous_function.tex',
-'/home/jezri/wits_calculus/piecewise_continuous_functions_are_intergratable.tex',
-'/home/jezri/wits_calculus/nonintergratable_function.tex'
-]
+from koneroj import makebook
+list=[ 'partition.tex',
+'partition_subinterval.tex',
+'norm_of_a_partition.tex',
+'riemann_sum.tex',
+'definite_integral.tex',
+'continues_functions_are_intergratable.tex',
+'piecewise_continuous_function.tex',
+'piecewise_continuous_functions_are_intergratable.tex',
+'nonintergratable_function.tex']
+
 intergration_rules = [
-
-'/home/jezri/wits_calculus/intergral_rules/order_of_intergration.tex',
-'/home/jezri/wits_calculus/intergral_rules/zero_width_interval.tex',
-'/home/jezri/wits_calculus/intergral_rules/intergration_constant.tex',
-'/home/jezri/wits_calculus/intergral_rules/intergration_sum_and_difference.tex',
-'/home/jezri/wits_calculus/intergral_rules/additivity.tex',
-'/home/jezri/wits_calculus/intergral_rules/max_min_inequality.tex',
-'/home/jezri/wits_calculus/intergral_rules/domination.tex'
-
-
+'intergral_rules/order_of_intergration.tex',
+'intergral_rules/zero_width_interval.tex',
+'intergral_rules/intergration_constant.tex',
+'intergral_rules/intergration_sum_and_difference.tex',
+'intergral_rules/additivity.tex',
+'intergral_rules/max_min_inequality.tex',
+'intergral_rules/domination.tex'
 ]
-list += intergration_rules
-list += ['/home/jezri/wits_calculus/area.tex',
-'/home/jezri/wits_calculus/mean_value_theorem_for_defininte_intergrals.tex',
-'/home/jezri/wits_calculus/antiderivites_intergral.tex' ,
-'/home/jezri/wits_calculus/intergral_of_odd_function.tex',
-'/home/jezri/wits_calculus/intergral_of_even_function.tex',
-'/home/jezri/wits_calculus/area_between_curves.tex'
 
+list += intergration_rules
+list += [
+'area.tex',
+'mean_value_theorem_for_defininte_intergrals.tex',
+'antiderivites_intergral.tex' ,
+'intergral_of_odd_function.tex',
+'intergral_of_even_function.tex',
+'area_between_curves.tex'
 ]
 volumn = [
-'/home/jezri/wits_calculus/volume/volumn.tex',
-'/home/jezri/wits_calculus/volume/volumn_by_disk.tex',
-'/home/jezri/wits_calculus/volume/volumn_by_shell.tex'
+'volume/volumn.tex',
+'volume/volumn_by_disk.tex',
+'volume/volumn_by_shell.tex'
 ]
 list += volumn
 name = 'calculus'
---
+++ wits_modeling/modeling.py
@@ -0,0 +1,9 @@
+from koneroj import makebook
+list=[ 'differentail_equation.tex',
+'differentail_solution.tex',
+'differentail_order.tex',
+'seperable_equations.tex' 
+]
+name = 'modeling'
+makebook(list,name)
+
