f7a03b26285a1856ea8362d4dad623613646f747
Jezri Krinsky <jezrikrinsky@gmail.com>
2017-07-24 23:16:10 +0200

    basic set builder notation added

---
+++ wits_dcs/dcs.py
@@ -0,0 +1,20 @@
+from koneroj import makebook 
+list =[
+'set.latex',
+'isAMember.latex',
+'subset.latex',
+'setOrder.latex',
+'properSubset.latex',
+'nullSet.latex',
+'cardinality.latex',
+'union.latex',
+'intersection.latex',
+'absorbtionUnion.latex',
+'absorbtionIntersection.latex',
+'unionCommutivity.latex',
+'intersectionCommutivity.latex',
+'associativity.latex',
+'distrabutivity.latex'
+]
+name = 'dcs'
+makebook(list,name)
